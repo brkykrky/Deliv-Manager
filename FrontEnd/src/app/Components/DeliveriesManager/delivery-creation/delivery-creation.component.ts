34e5d22951a14468ed665853454c8f0dae011f01
KellouchaSamah <samahklc1996@gmail.com>
2023-12-22 15:25:46 +0100

    feat : Adding the creation function to delivery

--- FrontEnd/src/app/Components/DeliveriesManager/delivery-creation/delivery-creation.component.ts
+++ FrontEnd/src/app/Components/DeliveriesManager/delivery-creation/delivery-creation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { delivery } from '../../../Interfaces/delivery';
+import { deliveryService } from '../../../services/delivery.service';
 
 @Component({
   selector: 'app-delivery-creation',
@@ -6,8 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './delivery-creation.component.css'
 })
 export class DeliveryCreationComponent {
-  value :any ;
 isChecked = true;
+ramassage = '';
+depot =  '';
+delivery : delivery = {
+  id: 0,
+  pickupAddress: '',
+  storageAddress: '',
+  assignedTour: null
+}
+constructor(public serviceDelivery : deliveryService){}
+
+onSubmit() {
+
+  this.delivery.pickupAddress=this.ramassage;
+  this.delivery.storageAddress=this.depot;
+  this.serviceDelivery.createdelivery(this.delivery).subscribe(
+    () => {
+      console.log('Delivery was added successfully');
+      window.location.reload();
+
+    },
+    (error) => {
+      console.error('Error adding delivery', error);
+    }
+  );
+  }
+
+
+
 
 
 }
