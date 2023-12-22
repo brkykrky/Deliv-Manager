accb9ea45753abff2e89ac4429e400d430fe8fa9
KellouchaSamah <samahklc1996@gmail.com>
2023-12-22 05:05:12 +0100

    feat: Adding services

---
+++ FrontEnd/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    apiUrl: "http://localhost:8080/",
+    client_id: "frontend",
+    realm: "m2gil",
+    keycloak_url: "https://localhost:8081/" ,
+    logout_redirect:"http://localhost:4200"
+}
