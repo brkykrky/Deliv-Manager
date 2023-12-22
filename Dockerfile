a55d8d9956c9e5cd5f7817c76bf93b75f6ad9587
brkykrky <berkaykarakayawp@gmail.com>
2023-12-22 13:10:34 +0100

    feat(CI/CD): all services dockerized

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,19 @@ FROM maven:3.9.6-eclipse-temurin-17-alpine AS maven
 
 WORKDIR /usr/src/app
 COPY . /usr/src/app
+
 # Compile and package the application to an executable JAR
 RUN mvn package
 
-RUN ls /usr/src/app/target/
 # For Java 17,
 FROM eclipse-temurin:17-jdk-alpine
 
-ARG JAR_FILE=delivmanager.jar
+ARG JAR_FILE=deliv.manager-0.0.1-SNAPSHOT.jar
 
 WORKDIR /opt/app
 
 # Copy the spring-boot-api-tutorial.jar from the maven stage to the /opt/app directory of the current stage.
-COPY --from=maven /usr/src/app/target/${JAR_FILE} /opt/app/
+COPY --from=maven /usr/src/app/target/deliv.manager-0.0.1-SNAPSHOT.jar /opt/app/
+COPY ./pom.xml /opt/app
 
-ENTRYPOINT ["java","-jar","delivmanager.jar"]
+ENTRYPOINT ["java","-jar","deliv.manager-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/berkay/karakaya/deliv/manager/configuration/SecurityConfig.java
+++ src/main/java/com/berkay/karakaya/deliv/manager/configuration/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.berkay.karakaya.deliv.manager.configuration;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -11,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -36,6 +42,7 @@ public class SecurityConfig {
                       }
                     }))
         .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .oauth2ResourceServer(
             (rs) -> rs.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
